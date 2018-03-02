3d3b531e5b3d6efe8c0d3009d0457c5e66b63774
DESKTOP-5MAUAOO\hp <1902348023@qq.com>
2018-03-02 14:57:38 +0800

    增加选择excel文件功能

--- src/excelEditer/ExcelEditer.java
+++ src/excelEditer/ExcelEditer.java
@@ -2,11 +2,11 @@ package excelEditer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JRadioButton;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -20,45 +20,47 @@ import java.awt.Font;
 import java.awt.Color;
 
 public class ExcelEditer extends JFrame implements ActionListener{
+	private static File file;
 	private static final int DEFAULT_WIDTH=500;
 	private static final int DEFAULT_HEIGHT=300;
+	private static JLabel ifLabel = new JLabel("源表格 ：");
+	private static JButton ifButton = new JButton("导入");
+	private static JRadioButton rb360 = new JRadioButton("360");
+	private static JRadioButton rbshenma = new JRadioButton("神马");
+	private static JRadioButton rbsougou = new JRadioButton("搜狗");
+	private static TextField ifText = new TextField();
 
 	public ExcelEditer() {
-		JLabel ifLabel = new JLabel("源表格：");
 		ifLabel.setFont(new Font("宋体", Font.BOLD, 12));
 		ifLabel.setBounds(20, 20, 60, 30);
-		this.add(ifLabel);
+		getContentPane().add(ifLabel);
 		
-		JButton ifButton = new JButton("导入");
 		ifButton.setFont(new Font("宋体", Font.BOLD, 12));
 		ifButton.setBounds(400, 20, 80, 30);
-		this.add(ifButton);
+		getContentPane().add(ifButton);
 		
-		JRadioButton rb360 = new JRadioButton("360");
 		rb360.setFont(new Font("宋体", Font.BOLD, 12));
 		rb360.setBounds(20, 60, 100, 30);
-		this.add(rb360);
+		getContentPane().add(rb360);
 		
-		JRadioButton rbshenma = new JRadioButton("神马");
 		rbshenma.setFont(new Font("宋体", Font.BOLD, 12));
 		rbshenma.setBounds(140, 60, 100, 30);
-		this.add(rbshenma);
+		getContentPane().add(rbshenma);
 		
-		JRadioButton rbsougou = new JRadioButton("搜狗");
 		rbsougou.setFont(new Font("宋体", Font.BOLD, 12));
 		rbsougou.setBounds(260, 60, 100, 30);
-		this.add(rbsougou);
+		getContentPane().add(rbsougou);
 		
-		TextField ifField = new TextField();
-		ifField.setBackground(Color.WHITE);
-		ifField.setEnabled(false);
-		ifField.setEditable(false);
-		ifField.setForeground(Color.BLACK);
-		ifField.setFont(new Font("宋体", Font.PLAIN, 12));
-		ifField.setBounds(80, 20, 300, 30);
-		this.add(ifField);
+		ifText.setText("请选择待转换excel文件...");
+		ifText.setBackground(Color.WHITE);
+		ifText.setEnabled(false);
+		ifText.setEditable(false);
+		ifText.setForeground(Color.BLACK);
+		ifText.setFont(new Font("宋体", Font.PLAIN, 14));
+		ifText.setBounds(80, 26, 300, 20);
+		getContentPane().add(ifText);
 		
-		this.setLayout(null);
+		getContentPane().setLayout(null);
 		this.setTitle("Excel Editer By Jozecn");
 		this.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -72,10 +74,30 @@ public class ExcelEditer extends JFrame implements ActionListener{
 
 	public static void main(String[] args) {
 		ExcelEditer obj = new ExcelEditer();
-		// 此处为Excel文件路径 
-        File file = new File("D:/JxlTest.xls");
-        obj.readExcel(file);
+		//obj.readExcel(file);
+		ifButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	ifButtonMethod();
+            }
+        });
 	}
+	
+	public static void ifButtonMethod(){
+		JFileChooser chooser = new JFileChooser(); //创建选择文件对象
+		chooser.setDialogTitle("请选择文件");//设置标题
+		chooser.setMultiSelectionEnabled(false);
+		chooser.setFileSelectionMode(0);//0表示只能选择文件，1表示只能选择文件夹，2表示均可选
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Excel(*xls)","xls");//定义可选择文件类型
+		chooser.setFileFilter(filter); //设置可选择文件类型
+		int returnVal = chooser.showOpenDialog(null); //打开选择文件对话框,null可设置为你当前的窗口JFrame或Frame
+	    if(JFileChooser.APPROVE_OPTION == returnVal){
+	    	//file = chooser.getSelectedFile(); //file为用户选择的图片文件
+		    String filepath = chooser.getSelectedFile().getAbsolutePath(); //获取绝对路径  
+		    ifText.setText(filepath);
+		    System.out.println(filepath);
+	    }
+    }
 	
 	public void readExcel(File file) {  
         try {  
